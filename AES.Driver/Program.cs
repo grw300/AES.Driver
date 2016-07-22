e6577257a747373a66bf5b85434f375b662b002a
Gregory Williams <grw300@gmail.com>
2016-07-21 19:52:56 -0500

    * AES.Driver.sln: Got the encrypt/decrypt working.
    * Program.cs: Got the AES encryption/decryption working
    * AES.Driver.csproj: Added Bouncy Castle

--- AES.Driver/Program.cs
+++ AES.Driver/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities.Encoders;
+
+using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace AES.Driver
 {
 	class MainClass
 	{
+		private static CbcBlockCipher cipher;
+		private static KeyParameter keyParameter;
+		private static byte[] encryptedText;
+		private static byte[] decryptedText;
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var p = Hex.Decode("8000 7000 6000 5000 4000 3000 2000 1000");
+			var k = Hex.Decode("0000 0000 0000 0000 0000 0000 0000 0001");
+
+
+			MainClass.cipher = new CbcBlockCipher(new AesEngine());
+			MainClass.keyParameter = new KeyParameter(k);
+
+			EncryptDecrypt(p, k);
+
+			Console.WriteLine(Regex.Replace(Hex.ToHexString(p), ".{4}", "$0 "));
+			Console.WriteLine(Regex.Replace(Hex.ToHexString(k), ".{4}", "$0 "));
+
+			Console.WriteLine(Regex.Replace(Hex.ToHexString(encryptedText), ".{4}", "$0 "));
+			Console.WriteLine(Regex.Replace(Hex.ToHexString(decryptedText), ".{4}", "$0 "));
+
+		}
+
+		public static void EncryptDecrypt(byte[] p, byte[] k)
+		{
+			cipher.Init(true, keyParameter);
+			encryptedText = new byte[p.Length];
+			cipher.ProcessBlock(p, 0, encryptedText, 0);
+
+			cipher.Init(false, keyParameter);
+			decryptedText = new byte[encryptedText.Length];
+			cipher.ProcessBlock(encryptedText, 0, decryptedText, 0);
 		}
 	}
 }
